e291156b5d6ee3f1d3162bf84b1b49b6f62d1256
Kuei-Jung Hu <kueijunghu@gmail.com>
2021-05-16 16:05:09 +0900

    Create the layout of the Instructions screen and the navigation to the Shoe List screen

--- shoe-store-inventory/app/src/main/java/com/udacity/shoestore/screens/InstructionsFragment.kt
+++ shoe-store-inventory/app/src/main/java/com/udacity/shoestore/screens/InstructionsFragment.kt
@@ -5,14 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragment = this
+    }
+
+    fun navigateToShoeListScreen() {
+        view?.findNavController()?.navigate(InstructionsFragmentDirections.actionInstructionsToShoeList())
     }
 }
