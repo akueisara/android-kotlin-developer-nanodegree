7bda3eee49fa087d25151c275d01702b947f3ab9
Kuei-Jung Hu <kueijunghu@gmail.com>
2021-05-16 15:18:24 +0900

    Create the layout of the Welcome screen and the navigation to the Instructions screen

--- shoe-store-inventory/app/src/main/java/com/udacity/shoestore/screens/WelcomeFragment.kt
+++ shoe-store-inventory/app/src/main/java/com/udacity/shoestore/screens/WelcomeFragment.kt
@@ -5,19 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragment = this
+    }
+
+    fun navigateToInstructionsScreen() {
+        view?.findNavController()?.navigate(WelcomeFragmentDirections.actionWelcomeToInstructions())
     }
 }
