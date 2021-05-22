1a13921df5156dcdeee27efa17928d25e9db0099
Kuei-Jung Hu <kueijunghu@gmail.com>
2021-05-23 01:08:28 +0900

    Create a list of clickable asteroids (fake data) on the Main screen

---
+++ asteroid-radar/app/src/main/java/com/udacity/asteroidradar/main/AsteroidListAdapter.kt
@@ -0,0 +1,61 @@
+package com.udacity.asteroidradar.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.databinding.ListItemAsteroidViewBinding
+
+class AsteroidListAdapter(private val clickListener: AsteroidListener): ListAdapter<Asteroid, AsteroidListAdapter.AsteroidViewHolder>(AsteroidDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidViewHolder {
+        return AsteroidViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: AsteroidViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(clickListener, item)
+    }
+
+    class AsteroidViewHolder private constructor(val binding: ListItemAsteroidViewBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(clickListener: AsteroidListener, item: Asteroid) {
+            binding.asteroid = item
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): AsteroidViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemAsteroidViewBinding.inflate(layoutInflater, parent, false)
+
+                return AsteroidViewHolder(binding)
+            }
+        }
+    }
+}
+
+/**
+ * Callback for calculating the diff between two non-null items in a list.
+ *
+ * Used by ListAdapter to calculate the minimum number of changes between and old list and a new
+ * list that's been passed to `submitList`.
+ */
+class AsteroidDiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+    override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class AsteroidListener(val clickListener: (asteroidId: Long) -> Unit) {
+    fun onClick(asteroid: Asteroid) = clickListener(asteroid.id)
+}
+
--- asteroid-radar/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ asteroid-radar/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,7 +2,9 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
@@ -13,12 +15,26 @@ class MainFragment : Fragment() {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
+
+        val adapter = AsteroidListAdapter(AsteroidListener { asteroidId ->
+            Toast.makeText(context, "$asteroidId", Toast.LENGTH_LONG).show()
+        })
+        binding.asteroidRecycler.adapter = adapter
+
+        viewModel.asteroids.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
 
         setHasOptionsMenu(true)
 
