b07dbbafa8520cbe4e127f6996e65c7522ad590b
Kuei-Jung Hu <kueijunghu@gmail.com>
2021-06-06 03:24:29 +0900

    Replace the API key

--- asteroid-radar/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ asteroid-radar/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "wxthVHrXBVZFK6x7nweX6eKsfPfV98V6BIFwYlTe"
+    const val API_KEY = "please_enter_your_api_key"
 }
