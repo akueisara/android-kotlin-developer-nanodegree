875980ac85745758a336d19bd6e309236d69b674
Kuei <akueisara39@gmail.com>
2021-08-09 21:13:10 +0900

    Create the details screen and display the name of the repository and status of the download

--- load-app/app/src/main/java/com/udacity/DetailActivity.kt
+++ load-app/app/src/main/java/com/udacity/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.udacity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
     companion object {
@@ -13,6 +14,14 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
-    }
 
+        if (intent.hasExtra(DOWNLOAD_OPTION_NAME)) {
+            file_name_detail_text_view.text = intent.getStringExtra(DOWNLOAD_OPTION_NAME)
+            status_name_detail_text_view.text = getString(R.string.success_status)
+        }
+
+        ok_button.setOnClickListener {
+            finish()
+        }
+    }
 }
