5fcb85f0efcf05ddb10e623375f87b5ecf672596
Kuei <akueisara39@gmail.com>
2021-08-09 14:02:23 +0900

    Modify the custom LoadingButton to show the downloading status

--- load-app/app/src/main/java/com/udacity/LoadingButton.kt
+++ load-app/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,6 +3,9 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 import kotlin.properties.Delegates
@@ -10,24 +13,52 @@ import kotlin.properties.Delegates
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private var buttonText = context.getString(R.string.button_name)
+
     private var widthSize = 0
     private var heightSize = 0
 
     private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+        when(new) {
+            ButtonState.Clicked -> TODO()
+            ButtonState.Completed -> {
+                buttonText = context.getString(R.string.button_name)
+            }
+            ButtonState.Loading -> {
+                buttonText = context.getString(R.string.button_loading)
+            }
+        }
     }
 
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        color = Color.WHITE
+    }
 
     init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.LoadingButton,
+            0, 0).apply {
+            try {
 
+            } finally {
+                recycle()
+            }
+        }
     }
-
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
+        canvas?.drawColor(context.getColor(R.color.colorPrimary))
+        textPaint.getTextBounds(buttonText,0, buttonText.length, Rect())
+        val textXPosition = width.toFloat() / 2
+        val textYPosition = height.toFloat() / 2 - (textPaint.descent() + textPaint.ascent()) / 2
+        canvas?.drawText(buttonText, textXPosition, textYPosition, textPaint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -43,4 +74,8 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    fun setLoadingButtonState(state: ButtonState) {
+        buttonState = state
+        invalidate()
+    }
 }
--- load-app/app/src/main/java/com/udacity/MainActivity.kt
+++ load-app/app/src/main/java/com/udacity/MainActivity.kt
@@ -3,12 +3,12 @@ package com.udacity
 import android.app.DownloadManager
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,11 +17,19 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var downloadManager: DownloadManager
     private var downloadID: Long = 0
+    private var downloading = false
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+
+    private val handler: Handler = Handler(Looper.getMainLooper())
+
+    private val queryProgressRunnable: Runnable = Runnable {
+        queryProgress()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         custom_button.setOnClickListener {
             download()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 
     private val receiver = object : BroadcastReceiver() {
@@ -50,12 +63,58 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        // enqueue puts the download request in the queue.
+        downloadID = downloadManager.enqueue(request)
+        if (downloadID != 0L) {
+            startQueryProgress()
+        }
+    }
+
+    private fun startQueryProgress() {
+        downloading = true
+        handler.post(queryProgressRunnable)
+    }
+
+    private fun stopQueryProgress() {
+        downloading = false
+        handler.removeCallbacks(queryProgressRunnable)
+    }
+
+    private fun queryProgress() {
+        val query = DownloadManager.Query().setFilterById(downloadID)
+        val cursor = downloadManager.query(query)
+        if (cursor != null && cursor.moveToFirst()) {
+            when(cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+                DownloadManager.STATUS_RUNNING -> {
+                    custom_button.setLoadingButtonState(ButtonState.Loading)
+                    val downloadedSize = cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                    val totalSize = cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+                    if (totalSize != -1L) {
+                        // TODO: Update the download progress of the LoadingButton
+                        Log.d(TAG, "Progress: ${(downloadedSize.toFloat() / totalSize) * 100}")
+                    }
+                }
+                DownloadManager.STATUS_SUCCESSFUL -> {
+                    custom_button.setLoadingButtonState(ButtonState.Completed)
+                    stopQueryProgress()
+                }
+                DownloadManager.STATUS_FAILED -> {
+                    custom_button.setLoadingButtonState(ButtonState.Completed)
+                    stopQueryProgress()
+                }
+            }
+        } else {
+            stopQueryProgress()
+        }
+        cursor.close()
+        if(downloading) {
+            handler.post(queryProgressRunnable)
+        }
     }
 
     companion object {
+        private val TAG = MainActivity::class.java.simpleName
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
